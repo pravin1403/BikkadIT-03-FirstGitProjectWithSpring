664b6c58070b39113a8e81e9736b1d9666535014
Pravin <pravinbhakar01@gmail.com>
2022-08-04 10:42:48 +0530

    First Git Project

---
+++ src/main/java/com/BikkadIT/FirstGitProjectApplication.java
@@ -0,0 +1,13 @@
+package com.BikkadIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstGitProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstGitProjectApplication.class, args);
+	}
+
+}
